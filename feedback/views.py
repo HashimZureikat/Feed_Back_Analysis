b4fd2a2d41d7033ae17cc76f8863361c2622b516
Hashim <hashimzureikat@Gmail.com>
2024-05-27 03:41:16 +0300

    full stack azure analyzer

--- feedback/views.py
+++ feedback/views.py
@@ -145,7 +145,7 @@ def analyze_feedback(request):
                 negative_score = doc.confidence_scores.negative
 
                 # Adjusting threshold for neutral sentiment
-                if neutral_score >= 0.02:
+                if neutral_score >= 0.06:
                     overall_sentiment = 'neutral'
                 elif positive_score >= 0.5:
                     overall_sentiment = 'positive'
