4ee33790f1b8f71a1295df507b6d7d85312a5c87
Hashim <hashimzureikat@Gmail.com>
2024-06-05 06:17:14 +0300

    full stack azure analyzer

--- ke.py
+++
@@ -1,6 +0,0 @@
-import secrets
-
-def generate_secret_key():
-    return ''.join(secrets.choice('abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)') for i in range(50))
-
-print(generate_secret_key())
